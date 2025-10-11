f79173b9d44e9b56749a20c12c3800f2649bbe87
Piyusha Kavle <kpiyu411@gmail.com>
2025-10-11 18:07:03 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SmartTransportSystem1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTransportSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTransportSystem1Application.class, args);
+	}
+
+}
